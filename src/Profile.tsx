9f6f84ab2cf19f41c645c232d010bcb356ac92ec
Mariusz Pilarek <mariusz@eiger.co>
2024-01-10 15:01:09 +0100

    Updated to latest viem changes.

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useAccount, useConnect, useDisconnect, useConnectorClient } from 'wagmi'
 import { zkSyncTestnet } from "viem_zksync_chains"
-import { sendEip712Transaction, writeEip712Contract } from 'viem_zksync_chains/zksync'
+import { eip712Actions } from 'viem_zksync_chains/zksync'
 import { simulateContract } from '@wagmi/core';
 import { readContract } from '@wagmi/core';
 import {
@@ -56,15 +56,16 @@ export function Profile({ config }: any) {
   const sendEip712T = async() => {
     const request = {
       chainId: defChain.id,
+      chain: defChain,
       account: address,
       to: '0x36615Cf349d7F6344891B1e7CA7C72883F5dc049',
       maxFeePerGas: 250000000n,
       maxPriorityFeePerGas: 0n,
       value: 100000,
-      gas: 318614,
       ...paymasterParams
     }
-    const hash = await sendEip712Transaction(walletClient, request)
+    const client = walletClient?.extend(eip712Actions);
+    const hash = await client.sendTransaction(request)
     console.log(hash)
   }
 
@@ -81,8 +82,8 @@ export function Profile({ config }: any) {
       account: address,
     })
 
-    console.log(request)
-    const hash = await writeEip712Contract(walletClient, request);
+    const client = walletClient?.extend(eip712Actions);
+    const hash = await client.writeContract(request)//walletClient.writeContract(request);
     console.log("Hash: " + hash)
   }
 
